eb0efda5c8e17e10e9d07c08e0a1bff987d8af36
Kyle Thornburgh <kyle.thornburgh@nreca.coop>
2014-08-07 17:01:15 -0400

    Update

--- Review/main.js
+++ Review/main.js
@@ -1,25 +1,6 @@
 angular.module('myApp', ['contactApp', 'slideshowApp'])
     .controller('myController', function ($scope) {
         $scope.greeting = '';
-        $scope.people = [
-            {name: 'John',
-                show: true},
-            {name: 'Jake',
-                show: true},
-            {name: 'Josh',
-                show: true},
-            {name: 'Jessa',
-                show: true},
-            {name: 'Joanne',
-                show: true},
-            {name: 'Bill',
-                show: true}
-        ];
-        $scope.remove = function(person) {
-            var index = people.indexOf(person);
-            $scope.people[index].splice(index, 1);
-            console.log(person);
-        };
     })
     .directive('myDirective', function() {
         return {
